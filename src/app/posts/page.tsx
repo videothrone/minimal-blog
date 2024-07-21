39bf95809550fb6083b5fcbfed51cf524ce04289
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-07-21 11:43:08 +0200

    💄 Adjust headline bottom margin of posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 export default async function Page() {
   return (
     <main className="text-center pt-16 px-5">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+      <h1 className="text-4xl md:text-5xl font-bold mb-7">All posts</h1>
 
       <Suspense fallback="Loading...">
         <PostsList />
