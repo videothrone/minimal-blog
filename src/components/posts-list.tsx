6e618720e12cb9a14ba7d3702365b229d559aae1
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-07-21 11:44:45 +0200

    ♻️ Add date and icon to posts-list items

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,14 +1,26 @@
 import prisma from "@/lib/db";
 import Link from "next/link";
+import { format } from "date-fns";
+import { CalendarDaysIcon } from '@heroicons/react/24/solid'
 
 export default async function PostsList() {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
 
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.id} className="mb-3">
+        <li key={post.id} className="mb-5 flex flex-col justify-center">
           <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            <small className="text-gray-500 flex items-center justify-center gap-1">
+              <CalendarDaysIcon className="w-5 h-5 text-gray-500" />
+              <div className="italic">
+                {format(new Date(post.createdAt), 'MMMM d, yyyy')}
+              </div>
+            </small>
         </li>
       ))}
     </ul>
