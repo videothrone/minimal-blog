c0b056f024643dda9070bfa0e6247a36b010af05
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-07-22 10:29:37 +0200

    ♻️ Add basic styling to dynamic post not-found page

--- src/app/posts/[id]/not-found.tsx
+++ src/app/posts/[id]/not-found.tsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div>No post found for that id.</div>;
+  return (
+    <main className="flex items-center justify-center mt-24">
+      <div>No post found for that id.</div>
+    </main>
+  )
 }
