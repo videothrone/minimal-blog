82e093ab9d1d70567603dde7c7203959731cda7a
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-08-03 14:33:06 +0200

    ♻️ Add parameter to PostsList to limit rendered posts

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 import { format } from "date-fns";
 import { CalendarDaysIcon } from '@heroicons/react/24/solid'
 
-export default async function PostsList() {
+export default async function PostsList({ limit }: { limit?: number } = {}) {
   const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: 'desc'
-    }
+    },
+    take: limit // Limit the results
   });
 
   return (
@@ -15,12 +16,12 @@ export default async function PostsList() {
       {posts.map((post) => (
         <li key={post.id} className="mb-5 flex flex-col justify-center">
           <Link href={`/posts/${post.id}`}>{post.title}</Link>
-            <small className="text-gray-500 flex items-center justify-center gap-1">
-              <CalendarDaysIcon className="w-5 h-5 text-gray-500" />
-              <div className="italic">
-                {format(new Date(post.createdAt), 'MMMM d, yyyy')}
-              </div>
-            </small>
+          <small className="text-gray-500 flex items-center justify-center gap-1">
+            <CalendarDaysIcon className="w-5 h-5 text-gray-500" />
+            <div className="italic">
+              {format(new Date(post.createdAt), 'MMMM d, yyyy')}
+            </div>
+          </small>
         </li>
       ))}
     </ul>
