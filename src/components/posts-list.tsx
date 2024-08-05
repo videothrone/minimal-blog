27e9390d8b12cd9e9f67002b5a8365208d9369cf
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-08-05 15:25:38 +0200

    ♻️ Add Edit button to PostsList component if logged in

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,21 +1,31 @@
 import prisma from "@/lib/db";
 import Link from "next/link";
 import { format } from "date-fns";
-import { CalendarDaysIcon } from '@heroicons/react/24/solid'
+import { CalendarDaysIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
+import { requireAuthentication } from "@/actions/actions";
 
 export default async function PostsList({ limit }: { limit?: number } = {}) {
   const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: 'desc'
     },
-    take: limit // Limit the results
+    take: limit
   });
+
+  const isAuthenticated = await requireAuthentication();
 
   return (
     <ul>
       {posts.map((post) => (
         <li key={post.id} className="mb-5 flex flex-col justify-center">
-          <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          <div className="flex justify-center gap-2">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            {isAuthenticated && (
+              <Link href={`/posts/${post.id}/edit`}>
+                <PencilSquareIcon className="w-5 h-5 text-blue-500" />
+              </Link>
+            )}
+          </div>
           <small className="text-gray-500 flex items-center justify-center gap-1">
             <CalendarDaysIcon className="w-5 h-5 text-gray-500" />
             <div className="italic">
